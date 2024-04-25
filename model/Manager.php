05830684ee6300e2d99e7a83d2e6f2cb06e3cf67
Dario0617 <dario.audige@logicsante.com>
2024-04-25 10:12:40 +0200

    secure DB connection

--- model/Manager.php
+++ model/Manager.php
@@ -9,15 +9,27 @@ class Manager
     private $_dsn = 'mysql:host=localhost:3306;dbname=';
     private $_login;
     private $_password;
-
     protected $manager;
 
     public function __construct()
     {
-        if( strstr( $_SERVER['HTTP_HOST'], '51.178.86.117' ) ) {
+        $envFilePath = dirname(__DIR__) . '/.env';
+        if (file_exists($envFilePath)){
+            $envData = file_get_contents($envFilePath, false);
+            $tmp = explode(";", $envData);
+            array_pop($tmp);
+            $list = [];
+            foreach($tmp as $elem){
+                $e = explode('=', $elem);
+                $list[$e[0]] = $e[1];                
+            }
+        } else {
+            die();
+        }
+        if( strstr( $_SERVER['HTTP_HOST'], $list['DB_HOST'] ) ) {
 			$this->_dsn .= 'dario_3';
-            $this->_login = 'dario';
-            $this->_password = 'dab3oeP-';
+            $this->_login = $list['DB_LOGIN'];
+            $this->_password = $list['DB_PASSWORD'];
         } else {
             $this->_dsn .= 'ticketing';
             $this->_login = 'root';
